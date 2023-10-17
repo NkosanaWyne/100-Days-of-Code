0bddb1883903e2b15816e9146158da647a60c43a
Nkosana Wyne <nkosanawyne@gmail.com>
2023-10-17 13:00:36 +0200

    Uploaded Tip Calculator from Repl

---
+++ main.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator.")
+
+#2. Ask the user for the city that they grew up in.
+question_1 = input("What's the name of the city you grew up in?\n")
+
+#3. Ask the user for the name of a pet.
+question_2 = input("What's your pets's name?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+band_name = print("Your band name could be " + question_1 + " " + question_2)
+
+print(band_name)
